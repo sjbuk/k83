8b4a722562be4a85dcb8b267e83bef96d3a400a6
sjbuk <stuartbonnell@gmail.com>
2023-03-26 15:42:35 +0100

    .

--- bootstrap/bootstrap.bat
+++ bootstrap/bootstrap.bat
@@ -1,5 +1,5 @@
 rem kubectl create namespace argocd
 rem kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 rem istioctl install --set profile=default
-kubectl apply -f https://github.com/sjbuk/k83/blob/a538eeb60f137d7377ca6f1139fe818b1daf4d7a/bootstrap/bootstrap.yaml?raw=true
+kubectl apply -f https://github.com/sjbuk/k83/blob/main/bootstrap/bootstrap.yaml?raw=true
 
