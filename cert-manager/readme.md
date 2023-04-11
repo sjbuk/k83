a661622692a68dd4b76e5de16c22c9ee803cd9e6
sjbuk <stuartbonnell@gmail.com>
2023-04-11 22:59:35 +0100

    Working cert manager

--- cert-manager/readme.md
+++ cert-manager/readme.md
@@ -1,1 +1,7 @@
 https://cert-manager.io/docs/tutorials/istio-csr/istio-csr/
+
+
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.11.0/cert-manager.yaml 
+kubectl -n kube-system create secret generic zero-sll-eabsecret — from-literal secret="aXsCDxJD8PnPoWg0kbnOjEFZ9c1lJ1Xjmrde4f3U6wSbzcL2O4EUHvXw8C3Go0jucZhu-CG7DVGw4gCyae6i-A"
+
+rem EAB KID 6yny0w0AvEqIwsxz2ZzTtw
--- vault/vault.bat
+++ vault/vault.bat
@@ -18,6 +18,35 @@ Initial Root Token: hvs.CK7EyyKu58H7MHiZP2KchWBE
 
 
 ## Unseal the first vault server until it reaches the key threshold
-kubectl exec -n vault --stdin=true --tty=true vault-0 -- vault operator unseal xIrTAngug1oQj39rtKxa+m3tRZTj87il/NsTpBlXGIUM
-kubectl exec -n vault --stdin=true --tty=true vault-0 -- vault operator unseal WXBBRx7akmw6hM8uIke7PgkDDljZ/aG4V4od8iPOvera
-kubectl exec -n vault --stdin=true --tty=true vault-0 -- vault operator unseal 65JecN2w7ZWZqybw6YIjilW0234zHiYBN5gR5Qra4TyG
+kubectl exec -n vault --stdin=true --tty=true vault-0 -- vault operator unseal wSpD17eB1BE1XHe+La0Zh8AI1rkTIg2Ali/2hn47PpLt
+kubectl exec -n vault --stdin=true --tty=true vault-0 -- vault operator unseal zLaGT4otuu546H6dEJLLxq5NBXwi5j0KO3VrTFfH8ldI
+kubectl exec -n vault --stdin=true --tty=true vault-0 -- vault operator unseal 80CLcaNx3bGtcPVzYAgqMSd/vEDjTuuUBzJFSNGhSuXN
+
+
+Unseal Key 1: wSpD17eB1BE1XHe+La0Zh8AI1rkTIg2Ali/2hn47PpLt
+Unseal Key 2: zLaGT4otuu546H6dEJLLxq5NBXwi5j0KO3VrTFfH8ldI
+Unseal Key 3: 80CLcaNx3bGtcPVzYAgqMSd/vEDjTuuUBzJFSNGhSuXN
+Unseal Key 4: gL+jHVijzz/fcgbAVvNUsGdDx85hW9XGQAmSIKTwFWIb
+Unseal Key 5: Jl4rcencUJ/gANCLWYG+KHSLVUqpH7aO0otTwAq2uFC3
+
+Initial Root Token: hvs.ukY6cXqIVrMIz6tPbnbzkxPX
+
+ACME
+dmBN-fErPJ_a5495Ia7xjA
+
+jP2uHZfj1j_Y7LQkVQWnX7neRORGPIjvTdYQbHjts5BmuUXGtMBHmKTgXAODB_El3lQnXjfF_2zVTZJzGE672w
+
+
+
+
+kubectl exec -n vault --stdin=true --tty=true vault-0 -- vault write pki/root/generate/internal common_name=kind.com  ttl=8760h
+
+vault write pki/root/generate/internal \
+    common_name=kind.com \
+    ttl=8760h
+
+    vault write pki/roles/kind-dot-com \
+    allowed_domains=example.com \
+    allow_subdomains=true \
+    max_ttl=72h
+
